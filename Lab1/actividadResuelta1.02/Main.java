7ecb0a534784f94d637d9361e30192845261265f
profeMelola <91023374+profeMelola@users.noreply.github.com>
2021-09-19 19:02:03 +0200

    Add files via upload

---
+++ Lab1/actividadResuelta1.02/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+   public static void main(String[] args) {
+      int num; 
+      System.out.print("Escriba un número: "); 
+      Scanner sc = new Scanner(System.in);
+      num = sc.nextInt(); 
+      System.out.println("Valor introducido: " + num); 
+   }
+}
---
+++ Lab1/actividadResuelta1.03/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+   public static void main(String[] args) {
+      int edad; 
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Escriba su edad: ");
+      edad = sc.nextInt();
+      edad = edad + 1; 
+      System.out.println("El próximo año su edad será: " + edad + " años");
+   }
+}
