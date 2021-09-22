426513ef0765ee2e0e0a44b08ec64b232f013308
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2021-09-22 12:12:58 +0200

    Update TiposDatosLogicos.java

--- Lab1/tiposdatos/TiposDatosLogicos.java
+++ Lab1/tiposdatos/TiposDatosLogicos.java
@@ -1,13 +1,9 @@
 /**
- * 
+ * Practicando con diferentes tipos de datos
  */
 package tiposdatos;
 
-/**
- * @author Openwebinars
- *
- */
-@SuppressWarnings("all")
+
 public class TiposDatosLogicos {
 
 	/**
@@ -15,25 +11,39 @@ public class TiposDatosLogicos {
 	 */
 	public static void main(String[] args) {
 
-		//OPERADORES LÓGICOS CONDICIONALES
-		int valor1 = 1;
+	//OPERADORES LÃ“GICOS CONDICIONALES
+	int valor1 = 1;
         int valor2 = 2;
-        //En el próximo capítulo profundizamos en las sentencias condicionales
+		
+        //PRIMEROS PASOS CON SENTENCIAS CONDICIONALES
         if((valor1 == 1) && (valor2 == 2))
             System.out.println("valor1 es 1 AND valor2 es 2");
         if((valor1 == 1) || (valor2 == 1))
             System.out.println("valor1 es 1 OR valor2 es 1");
 		
-        
+        // ------------------------------------------
         //OPERADOR "TERNARIO"
+	/* EJEMPLO
+	  Caso 1 (simple):
+	  
+	  if (x>y)
+   		mayor = x;
+	   else
+   		mayor = y;
+	
+	  Caso 2 (ternario):
+	  	mayor=(x>y)?x:y;
+	  */
+	
+	// Â¿QuÃ© valor pensÃ¡is que va a contener result?
         int result;
         boolean someCondition = false;
         result = someCondition ? valor1 : valor2;
         System.out.println(result);
+	// ------------------------------------------
         
         
-        //OPERADORES LÓGICOS RELACIONALES
-        //En el próximo capítulo profundizamos en las sentencias condicionales
+        //OPERADORES LÃ“GICOS RELACIONALES
         if(valor1 == valor2)
             System.out.println("valor1 == valor2");
         if(valor1 != valor2)
@@ -44,11 +54,6 @@ public class TiposDatosLogicos {
             System.out.println("valor1 < valor2");
         if(valor1 <= valor2)
             System.out.println("valor1 <= valor2");
-        
-		
 
 	}
-	
-	
-
 }
