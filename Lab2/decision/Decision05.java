8b3359e00e541863f29e458c2e0bc605102d1c91
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2021-09-24 12:32:06 +0200

    Update Decision05.java

--- Lab2/decision/Decision05.java
+++ Lab2/decision/Decision05.java
@@ -3,10 +3,6 @@
  */
 package decision;
 
-/**
- * @author Openwebinars
- *
- */
 public class Decision05 {
 
 	/**
@@ -15,37 +11,35 @@ public class Decision05 {
 	public static void main(String[] args) {
 		
 		int mes = 0;
-        String mesString;
-        switch (mes) {
-            case 1:  mesString = "Enero"; // mes == 1 -> true
-                     break;
-            case 2:  mesString = "Febrero";
-                     break;
-            case 3:  mesString = "Marzo";
-                     break;
-            case 4:  mesString = "Abril";
-                     break;
-            case 5:  mesString = "Mayo";
-                     break;
-            case 6:  mesString = "Junio";
-                     break;
-            case 7:  mesString = "Julio";
-                     break;
-            case 8:  mesString = "Agosto";
-                     break;
-            case 9:  mesString = "Septiembre";
-                     break;
-            case 10: mesString = "Octubre";
-                     break;
-            case 11: mesString = "Noviembre";
-                     break;
-            case 12: mesString = "Diciembre";
-                     break;
-            default: mesString = "Mes no v·lido";
-                     break;
-        }
-        System.out.println(mesString);
-
+        	String mesString;
+        	switch (mes) {
+            		case 1:  mesString = "Enero"; // mes == 1 -> true
+                     		break;
+            		case 2:  mesString = "Febrero";
+                     		break;
+            		case 3:  mesString = "Marzo";
+                     		break;
+            		case 4:  mesString = "Abril";
+                     		break;
+            		case 5:  mesString = "Mayo";
+                     		break;
+            		case 6:  mesString = "Junio";
+                     		break;
+            		case 7:  mesString = "Julio";
+                     		break;
+            		case 8:  mesString = "Agosto";
+                     		break;
+            		case 9:  mesString = "Septiembre";
+                     		break;
+            		case 10: mesString = "Octubre";
+                     		break;
+            		case 11: mesString = "Noviembre";
+                     		break;
+            		case 12: mesString = "Diciembre";
+                     		break;
+            		default: mesString = "Mes no v√°lido";
+                     		break;
+        	}
+        	System.out.println(mesString);
 	}
-
 }
