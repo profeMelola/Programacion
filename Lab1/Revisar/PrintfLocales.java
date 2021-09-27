ea5223a8423381cb3e19f438bb611b54f8d9a46f
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2021-09-27 11:33:27 +0200

    Create PrintfLocales.java

---
+++ Lab1/Revisar/PrintfLocales.java
@@ -0,0 +1,15 @@
+/**
+ * Revisar printf
+ * Revisar clase Locale
+ */
+import Java.util.Locale;
+
+public class PrintfLocales {
+
+    public static void main(String args[]) {
+        System.out.printf("%.2f: Default locale\n", 3.1415926535);
+        System.out.printf(Locale.GERMANY, "%.2f: Germany locale\n", 3.1415926535);
+        System.out.printf(Locale.US, "%.2f: US locale\n", 3.1415926535);
+    }
+
+}
