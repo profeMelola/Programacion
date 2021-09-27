be69f393a67dd78ba76870eb56908fdd17e57771
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2021-09-27 13:11:37 +0200

    Create BucleForChar2.java

---
+++ Lab1/Revisar/BucleForChar2.java
@@ -0,0 +1,9 @@
+public class BucleForChar2 {
+  public static void main(String args[]) {
+    char b;
+    for(b = 0x0041; b < 0x0045; b++) {
+	    System.out.println(b);
+    }
+    System.out.println("FUERA DEL BUCLE");
+  }
+}
