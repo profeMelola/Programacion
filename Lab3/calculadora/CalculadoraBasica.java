de1c45a1ef5b84ec00699b0ddcb52ae7922e8d3a
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2021-10-06 14:35:11 +0200

    Update and rename Calculadora.java to CalculadoraBasica.java

--- Lab3/calculadora/CalculadoraBasica.java
+++ Lab3/calculadora/CalculadoraBasica.java
@@ -1,18 +1,18 @@
 /**
- * Ejemplo m·s complejo que nos permite utilizar
+ * Ejemplo m√°s complejo que nos permite utilizar
  * 
- *  - Variables numÈricas
+ *  - Variables num√©ricas
  *  - Operadores
  *  - Expresiones
- *  - Estructuras de decisiÛn
- *  - Estructuras de repeticiÛn
+ *  - Estructuras de decisi√≥n
+ *  - Estructuras de repetici√≥n
  * 
  */
 package calculadora;
 
 import java.util.Scanner;
 
-public class Calculadora {
+public class CalculadoraBasica {
 
 	/**
 	 * @param args
@@ -20,17 +20,17 @@ public class Calculadora {
 	public static void main(String[] args) {
 		
 		//Variable donde almacenamos el valor seleccionado
-		//del men˙
+		//del men√∫
 		int opcion = 0;
 		//Por ahora, es normal si no se comprenden estas dos
-		//lÌneas de cÛdigo.
+		//l√≠neas de c√≥digo.
 		Scanner sc;
 		sc = new Scanner(System.in);
 		
 		do {
 
-			//Imprimimos por consola el men˙
-			System.out.println("\n"); //Este caracter especial nos permite imprimir una lÌnea en blanco
+			//Imprimimos por consola el men√∫
+			System.out.println("\n"); //Este caracter especial nos permite imprimir una l√≠nea en blanco
 			System.out.println("***** CALCULADORA *****");
 			System.out.println("1. Sumar");
 			System.out.println("2. Restar");
@@ -38,19 +38,19 @@ public class Calculadora {
 			System.out.println("4. Dividir");
 			System.out.println("5. Resto");
 			System.out.println("0. Salir del programa");
-			System.out.print("Introduzca una opciÛn v·lida: ");
+			System.out.print("Introduzca una opci√≥n v√°lida: ");
 			
-			//Estas lÌneas de cÛdigo nos permiten leer del teclado
-			//y transformar el valor leÌdo de String a int
+			//Estas l√≠neas de c√≥digo nos permiten leer del teclado
+			//y transformar el valor le√≠do de String a int
 			
 			// 1) Recoger el valor escrito en el teclado
 			// 2) Transformar esa cadena de caracteres en un valor entero
 			opcion = Integer.parseInt(sc.nextLine());
 			
 			//Si la opcion es menor que 0 o mayor que 5, no es una
-			//opciÛn v·lida
+			//opci√≥n v√°lida
 			if (opcion < 0 || opcion > 5) {
-				System.out.println("OpciÛn no v·lida. Vuelva a escoger");
+				System.out.println("Opci√≥n no v√°lida. Vuelva a escoger");
 				//Si la opcion es diferente a cero, solicitamos los 
 				//dos operandos
 			} else if (opcion != 0) {
@@ -75,11 +75,11 @@ public class Calculadora {
 				case 2: //Resta
 					System.out.println("El resultado de la resta es " + (operando1 - operando2));
 					break;
-				case 3: //MultiplicaciÛn
-					System.out.println("El resultado de la multiplicaciÛn es " + (operando1 * operando2));
+				case 3: //Multiplicaci√≥n
+					System.out.println("El resultado de la multiplicaci√≥n es " + (operando1 * operando2));
 					break;
-				case 4: //DivisiÛn
-					System.out.println("El resultado de la divisiÛn es " + (operando1 / operando2));
+				case 4: //Divisi√≥n
+					System.out.println("El resultado de la divisi√≥n es " + (operando1 / operando2));
 					break;
 				case 5: //Resto
 					System.out.println("El resto de dividir " +operando1 + " entre " + operando2 + " es "+ (operando1 % operando2));
@@ -91,10 +91,10 @@ public class Calculadora {
 			
 		} while (opcion != 0); //opcion == 0 implicar salir del programa
 		
-		//Indicamos que no vamos a leer ning˙n valor m·s por teclado.
+		//Indicamos que no vamos a leer ning√∫n valor m√°s por teclado.
 		sc.close();
 		
-		System.out.println("Finalizando la ejecuciÛn de la calculadora");
+		System.out.println("Finalizando la ejecuci√≥n de la calculadora");
 
 	}
 
