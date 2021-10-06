8ff320707b311077b79883360fbfa657ca86a0c3
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2021-10-06 14:29:15 +0200

    Update TiposDatosNumericos.java

--- Lab1/tiposdatos/TiposDatosNumericos.java
+++ Lab1/tiposdatos/TiposDatosNumericos.java
@@ -3,8 +3,8 @@
  */
 package tiposdatos;
 
-@SuppressWarnings("all")
-public class TiposDatosNumericos {
+
+public class TiposDatos {
 
 	/**
 	 * @param args
@@ -23,8 +23,10 @@ public class TiposDatosNumericos {
 		byte by = 100;
 		//Número 1000
 		short sh = 1000;
+                //short sh1 = 1000000; //da error
 		//Número 1000000
 		int in = 1000000;
+                //int in2 = 10000000000; //da error
 		
 		//Valor 26, en decimal
 		int decVal = 26;
@@ -49,14 +51,18 @@ public class TiposDatosNumericos {
 		//[-2^63,(2^63)-1]
 		long l = 3_000_000_000L;
 		
-		
+		// --------------------------------------------
 		//TIPOS DE DATOS REALES
 		//Precisión simple
 		float f = 0.25f;
-                float f2 = 0.25;
+                //float f2 = 0.25; //Esto da error
+                float f3 = 010; //en binario
+                float f4 = 0xff; //en hexadecimal
+                
 		//Precisión doble
 		double Pi = Math.PI;
-		
+		double d = 0.25;
+                double d2 = 0.25d;
 
                 // ----------------------------------
 		//OPERADORES NUMÉRICOS
@@ -100,16 +106,24 @@ public class TiposDatosNumericos {
 		System.out.println(z);
 		System.out.println(x);
 		
-		
-		int num1 = 5;
+		// ---------------------- INTERESANTE DECIMALES -----------------------
+		System.out.println("************** EJEMPLOS DECIMALES ****************");
+                int num1 = 5;
 		int num2 = 9;
 		
-		System.out.println("División con decimales (double):"+(double)num1/num2);
-                System.out.println("División con decimales (int):"+num1/num2);
+		System.out.println("* División con decimales (double):"+(double)num1/num2);
+                System.out.println("* División con decimales (int):"+num1/num2);
                 
-		
+                //En este caso no hace falta forzar un casting
+                //Cualquier operación aritmética entre dos tipos de datos distintos, 
+                //produce como resultado un tipo de datos igual al tipo mayor:
+                float float1= 0.25f;
+                System.out.println("* División con decimales (float):"+((num1+num2)*float1));
+                // -----------------------------------------------------------
+                
+		// -----------------------------------------------------------
 		//OPERADORES A NIVEL DE BITS
-                System.out.println("** OPERADORES A NIVEL DE BITS!!!!");
+                System.out.println("*********** OPERADORES A NIVEL DE BITS******************");
 		int bitmask = 0b0011; 
 		int val = 0b1111; 
 		
@@ -147,7 +161,92 @@ public class TiposDatosNumericos {
 		res = ~val; //11111111111111111111111111110000
 		System.out.print("Inverso o complementario ");
 		System.out.println(Integer.toBinaryString(res));
-                                
+                // -----------------------------------------------------------         
 		
+                
+                // EJEMPLOS DE CASTING IMPLÍCITO
+                System.out.println("********* EJEMPLO DE CASTING IMIPLÍCITO ***************");
+                //Ambos literales son literales de int, pero cumplen el rango de cada tipo
+                byte varPequena = 127; 
+                short varGrande = 12365;
+
+                //Convierto de literal int a variable long
+                long enteroGrande = 24556;
+
+                //Convierto de tipo long a tipo float
+                float decimalSimple = enteroGrande;
+
+                //Convierto de float a double
+                double decimalDoble = decimalSimple;
+
+                //Convierto de literal float a double
+                double decimalDoble2 = 3.14F;  
+                System.out.println("* varPequena: "+varPequena);
+                System.out.println("* varGrande: "+varGrande);
+                System.out.println("* enteroGrande: "+enteroGrande);
+                System.out.println("* decimalSimple: "+decimalSimple);
+                System.out.println("* decimalDoble: "+decimalDoble);
+                System.out.println("* decimalDoble2: "+decimalDoble2);
+                
+                System.out.println("********* EJEMPLO DE CASTING EXPLÍCITO ***************");
+                //una variable int ocupa 4 bytes, short 2 bytes
+                //int entero = 13;
+                int entero = 1_000_000_000;
+                //int entero_error = 10_000_000_000;
+                System.out.println("* entero: "+entero);
+                short enteroPequeno = (short)entero;
+                //short enteroPequeno = entero; //da error
+                System.out.println("* enteroPequeno: "+enteroPequeno);
+
+                long numeroLargo = 13;
+                System.out.println("* numeroLargo: "+numeroLargo);                
+                entero = (int)numeroLargo;
+                System.out.println("* entero: "+entero);
+                
+                double decimal = 23423.234234;
+                System.out.println("* decimal: "+decimal);
+                entero = (int) decimal;
+                System.out.println("* entero: "+entero);
+                
+                System.out.println("************* EJEMPLO CON CHARS *****************");
+                //Casting implícito
+                char letraA = 65; //valor ASCII -> 'A'
+                System.out.println("* letraA: "+letraA);
+                int valorA = letraA;
+                System.out.println("* valorA :"+valorA);
+
+                int valorB = 'B'; //entero ASCII -> 66
+                System.out.println("* valorB: "+valorB);
+
+                //Casting explícito
+                char letraB = (char)valorB; //convierto de int a char    
+                System.out.println("* letraB: "+letraB);
+                
+                // ------------------- PÉRDIDA DE DATOS --------------
+                System.out.println("************ EJEMPLO DE PÉRDIDA DE DATOS ***********");
+                int entero2byte = 263; 
+                System.out.println("* entero2byte: "+entero2byte);
+                byte numeroByte = (byte)entero;
+                System.out.println("* numeroByte: "+numeroByte);
+                
+                //--------------- UNICODE ---------------
+                // https://javautodidacta.es/tipos-primitivos-en-java-2/
+                System.out.println("************* UNICODE **************");
+                char caracter1 = 'Я';
+		char caracter2 = 1071; //DECIMAL
+		char caracter3 = 0b10000101111; //BINARIO
+		char caracter4 = 02057; //OCTAL
+		char caracter5 = 0x042F; //HEXADECIMAL
+		char caracter6 = '\u042F'; //UNICODE
+		char caracter7 = '4';
+ 
+		System.out.println(caracter1);  // я
+		System.out.println(caracter2);  // я
+		System.out.println(caracter3);  // я
+		System.out.println(caracter4);  // я
+		System.out.println(caracter5);  // я
+		System.out.println(caracter6);  // я
+		System.out.println(caracter7);  // 4                
+                
 	}
 }
