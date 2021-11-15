378f58519f002f2bc0bc40a9ca4d9cc98398ca65
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2021-11-15 18:45:32 +0100

    Add files via upload

---
+++ Lab4/Array/Bidimensional/notasAlumnos/ArraydeArrays/Notas.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package notasalumnosmejora3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author melola
+ */
+public class Notas {
+
+    public static void crearMatrizNotas(double[][] notasAlumnos, Scanner sc) {
+        for (int i = 0; i < notasAlumnos.length; i++) {
+            System.out.println("\t*Indica el número de notas del alumno <" + (i + 1) + ">");
+            int numNotas = sc.nextInt();
+            double[] notas = new double[numNotas];
+            notasAlumnos[i] = notas;
+        }
+    }
+    
+    public static void guardarNotasAlumnos(double[][] notasAlumnos, Scanner sc){
+        for (int i= 0 ; i < notasAlumnos.length; i++){
+            System.out.println("Notas del alumno "+ (i+1));
+            for (int j=0; j < notasAlumnos[i].length;j++){
+                System.out.println("\tEscribe la nota <"+(j+1)+"> :");
+                notasAlumnos[i][j] = sc.nextDouble();
+            }
+        }
+    }
+    
+    public static void pintaNotasAlumnos(double[][] notasAlumnos){
+        int contador=1;
+        for (double[] alumno: notasAlumnos){
+            System.out.println("*Notas del alumno "+contador);
+            System.out.println(Arrays.toString(alumno));
+            contador++;
+        }
+    }
+    
+    public static double obtenerNotaMediaAlumno(double[][] notasAlumnos, Scanner sc){
+        System.out.println("* Escribe el número del alumno del que quieres obtener la nota media:");
+        int numAlumno = sc.nextInt();
+        
+        double[] notas = notasAlumnos[numAlumno - 1];
+        
+        double media,suma = 0;
+        //así recorro las notas de cada alumno y las sumo
+        for (int i=0; i< notas.length; i++){
+            suma += notas[i];
+        }
+        
+        media = suma / notas.length;
+        
+        return media;
+        
+    }
+
+}
---
+++ Lab4/Array/Bidimensional/notasAlumnos/ArraydeArrays/NotasAlumnosMejora3.java
@@ -0,0 +1,34 @@
+package notasalumnosmejora3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author melola
+ */
+public class NotasAlumnosMejora3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("* Indica el número de alumnos de los que vas a guardar la nota:");
+        
+        int numAlumnos = sc.nextInt();
+        
+        double[][] notasAlumnos = new double[numAlumnos][];
+        
+        Notas.crearMatrizNotas(notasAlumnos,sc);
+        
+        Notas.guardarNotasAlumnos(notasAlumnos, sc);
+        
+        Notas.pintaNotasAlumnos(notasAlumnos);
+        
+        System.out.println("***** NOTA MEDIA\n"+Notas.obtenerNotaMediaAlumno(notasAlumnos, sc));
+        
+    }
+    
+}
