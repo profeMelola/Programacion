afe1bd183726926d5079138a9414e0523ddabb90
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2022-01-12 14:28:57 +0100

    Add files via upload

---
+++ RepasoNavidades/BolasJuegoAzar/src/es/daw/bolasJuegoAzar/JuegoAzarApp.java
@@ -0,0 +1,22 @@
+
+package es.daw.bolasJuegoAzar;
+
+import es.daw.bolasJuegoAzar.model.Ronda;
+
+/**
+ *
+ * @author natali
+ */
+public class JuegoAzarApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Ronda ronda =new Ronda();
+        System.out.println("RONDA:" +  ronda.toString());
+        
+    }
+    
+}
